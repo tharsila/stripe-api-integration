ea3a7b2bb9896ec03a8f2adfdfa2848880ae6e58
tharsila <tharsilapaula@gmail.com>
2024-05-24 22:08:13 -0300

    feat: adiciona config

---
+++ src/config/config.ts
@@ -0,0 +1,7 @@
+export const config = {
+    stripe: {
+        publisherKey: process.env.PUBLISHER_KEY,
+        secretKey: process.env.SECRET_KEY,
+        proPriceID: process.env.PRO_PRICE_ID,
+    },
+};
