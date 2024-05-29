d47d4717daab47a308ef2dd349267b9271363068
tharsila <tharsilapaula@gmail.com>
2024-05-29 00:00:13 -0300

    refactor: atualiza injeção de dependências do StripeController

--- src/routes/stripe.routes.ts
+++ src/routes/stripe.routes.ts
@@ -1,14 +1,16 @@
-
 import StripeController from '@/controllers/stripe.controller';
+import { UserRepository } from '@/repositories/user.repository';
+import { StripeService } from '@/services/stripe.service';
 import { Router } from 'express';
 
 const router = Router();
-
-const checkoutController = new StripeController();
+const userRepository = new UserRepository();
+const stripeService = new StripeService(userRepository);
+const stripeController = new StripeController(stripeService);
 
 router.post(
     '/checkout/:userId',
-    checkoutController.create.bind(checkoutController)
+    stripeController.create.bind(stripeController)
 );
 
 export default router;
