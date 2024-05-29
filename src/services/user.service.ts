09a0e74a969ff7fef0f3ef379bbe5f3749097d9e
tharsila <tharsilapaula@gmail.com>
2024-05-29 00:03:02 -0300

    refactor: refatora o `UserService` para integrar a criação de clientes do Stripe quando um novo usuário é criado

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,31 +1,36 @@
 // src/services/UserService.ts
 import { UserRepository } from '../repositories/user.repository';
 import { User } from '../models/user.model';
+import { StripeService } from './stripe.service';
 
 /* const userRepository = new UserRepository(); */
 
 export class UserService {
-    constructor(private userRepository: UserRepository) {}
+    constructor(
+        private userRepository: UserRepository,
+        private stripeService: StripeService
+    ) {}
 
     async getAllUsers(): Promise<User[]> {
         return await this.userRepository.getAll();
     }
 
-    async getUserById(id: number): Promise<User | null> {
+    async getUserById(id: string): Promise<User | null> {
         return await this.userRepository.getById(id);
     }
 
     async createUser(data: User): Promise<User> {
         const { name, email } = data;
-        return await this.userRepository.create(name, email);
+        const customer = await this.stripeService.createStripeCustomer(email, name);
+        return await this.userRepository.create(name, email, customer.id);
     }
 
-    async updateUser(id: number, data: User): Promise<User | null> {
+    async updateUser(id: string, data: User): Promise<User | null> {
         const { name, email } = data;
         return await this.userRepository.update(id, name, email);
     }
 
-    async deleteUser(id: number): Promise<void> {
+    async deleteUser(id: string): Promise<void> {
         await this.userRepository.remove(id);
     }
 }
