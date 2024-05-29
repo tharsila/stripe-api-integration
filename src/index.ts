37df1586b1c244fc4136ce52c6d6ab152217a390
tharsila <tharsilapaula@gmail.com>
2024-05-29 00:04:02 -0300

    feat: adicionar rota para webhooks do Stripe

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import userRoutes from './routes/user.routes';
 import stripeRoutes from './routes/stripe.routes';
+import stripeWebhookRoutes from './routes/stripe-webhook.routes';
 
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use('/stripe', stripeWebhookRoutes);
 app.use(express.json());
 app.use('/api', userRoutes);
 app.use('/stripe', stripeRoutes);
