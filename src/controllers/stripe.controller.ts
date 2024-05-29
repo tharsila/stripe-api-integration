f479ab7f7513f7be63cfe6992a3d2c8ac97ebc50
tharsila <tharsilapaula@gmail.com>
2024-05-28 23:57:04 -0300

    feat: refatoração do StripeController e adição de webhook

--- src/controllers/stripe.controller.ts
+++ src/controllers/stripe.controller.ts
@@ -1,14 +1,21 @@
-import { createCheckoutSession } from '@/repositories/stripe.repository';
+import { config } from '@/config/config';
+import { StripeService } from '@/services/stripe.service';
 import { Request, Response } from 'express';
+import Stripe from 'stripe';
 
 class StripeController {
+    constructor(private readonly stripeService: StripeService) {}
     async create(req: Request, res: Response) {
         const userId = req.params.userId;
+        const email = req.body.email;
 
-        const checkout = await createCheckoutSession(userId);
-        console.log(checkout);
-
+        const checkout = await this.stripeService.createCheckoutSession(userId, email);
         return res.send(checkout);
+    }
+
+    async webhook(req: Request, res: Response) {
+        const webhook = await this.stripeService.webhook(req, res);
+        return webhook
     }
 }
 
