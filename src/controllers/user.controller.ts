f0b86ee783e96d10d9493b3b20282725fedf80a1
tharsila <tharsilapaula@gmail.com>
2024-05-28 23:54:07 -0300

    style: formata codigo

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,37 +1,37 @@
 import { Request, Response } from 'express';
-import {UserService} from '../services/user.service';
+import { UserService } from '../services/user.service';
 
 class UserController {
-  constructor(private readonly userService: UserService) {}
+    constructor(private readonly userService: UserService) {}
 
-  async getAll(req: Request, res: Response): Promise<void> {
-    const users = await this.userService.getAllUsers();
-    res.json(users);
-  }
-
-  async getById(req: Request, res: Response): Promise<void> {
-    const user = await this.userService.getUserById(Number(req.params.id));
-    if (user) {
-      res.json(user);
-    } else {
-      res.status(404).json({ message: 'User not found' });
+    async getAll(req: Request, res: Response): Promise<void> {
+        const users = await this.userService.getAllUsers();
+        res.json(users);
     }
-  }
 
-  async create(req: Request, res: Response): Promise<void> {
-    const user = await this.userService.createUser(req.body);
-    res.status(201).json(user);
-  }
+    async getById(req: Request, res: Response): Promise<void> {
+        const user = await this.userService.getUserById(req.params.id);
+        if (user) {
+            res.json(user);
+        } else {
+            res.status(404).json({ message: 'User not found' });
+        }
+    }
 
-  async update(req: Request, res: Response): Promise<void> {
-    const user = await this.userService.updateUser(Number(req.params.id), req.body);
-    res.json(user);
-  }
+    async create(req: Request, res: Response): Promise<void> {
+        const user = await this.userService.createUser(req.body);
+        res.status(201).json(user);
+    }
 
-  async delete(req: Request, res: Response): Promise<void> {
-    await this.userService.deleteUser(Number(req.params.id));
-    res.status(204).send();
-  }
+    async update(req: Request, res: Response): Promise<void> {
+        const user = await this.userService.updateUser(req.params.id, req.body);
+        res.json(user);
+    }
+
+    async delete(req: Request, res: Response): Promise<void> {
+        await this.userService.deleteUser(req.params.id);
+        res.status(204).send();
+    }
 }
 
 export default UserController;
