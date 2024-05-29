3548113553c190ea46dfafcdf8d0f269464f3f3f
tharsila <tharsilapaula@gmail.com>
2024-05-29 00:01:18 -0300

    refactor: atualiza a configuração das rotas para injetar `StripeService` no `UserService`

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -3,11 +3,15 @@ import express, { Router } from 'express';
 import UserController from '../controllers/user.controller';
 import { UserService } from '@/services/user.service';
 import { UserRepository } from '@/repositories/user.repository';
+import { StripeService } from '@/services/stripe.service';
+
 
 const router = Router();
-const userRepository = new UserRepository()
-const userService = new UserService(userRepository);
-const userController = new UserController(userService); 
+
+const userRepository = new UserRepository();
+const stripeService = new StripeService(userRepository);
+const userService = new UserService(userRepository, stripeService);
+const userController = new UserController(userService);
 
 router.get('/users', userController.getAll.bind(userController));
 router.get('/users/:id', userController.getById.bind(userController));
