f9f44e4330c5b2a5a2c7cb0a8a08dd3a075fe14f
tharsila <tharsilapaula@gmail.com>
2024-05-31 13:09:14 -0300

    feat: adicionar rota para tasks

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import userRoutes from './routes/user.routes';
+import taskRoutes from './routes/task.routes';
 import stripeRoutes from './routes/stripe.routes';
 import stripeWebhookRoutes from './routes/stripe-webhook.routes';
 
@@ -9,6 +10,7 @@ const port = process.env.PORT || 3001;
 app.use('/stripe', stripeWebhookRoutes);
 app.use(express.json());
 app.use('/api', userRoutes);
+app.use('/api/', taskRoutes);
 app.use('/stripe', stripeRoutes);
 
 app.listen(port, () => {
