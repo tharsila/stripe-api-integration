3a3a09409ef91b5c567cc76f3e8068be92f3e938
tharsila <tharsilapaula@gmail.com>
2024-05-31 13:03:08 -0300

    feat: adiciona plano free

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,7 +2,14 @@ export const config = {
     stripe: {
         publisherKey: process.env.PUBLISHER_KEY,
         secretKey: process.env.SECRET_KEY,
-        proPriceID: process.env.PRO_PRICE_ID,
-        webhookSecret: process.env.WEBHOOK_SECRET
+        webhookSecret: process.env.WEBHOOK_SECRET,
+        plans: {
+            free: {
+                freePriceID: process.env.FREE_PRICE_ID,
+            },
+            pro: {
+                proPriceID: process.env.PRO_PRICE_ID,
+            },
+        },
     },
 };
